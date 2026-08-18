8ae6bca6261a770d4e5f171a22fae0bb17eac93d
Hita06 <hitaviswanathan@gmail.com>
2026-08-18 23:29:34 +1200

    Add sign up page and update navigation
    Created the Reserve Health sign up page, added styling, and updated homepage navigation links.

---
+++ CODE/HospitalManagementSystem/Pages/Signup.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HospitalManagementSystem.Pages
+{
+    public class SignupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+        }
+    }
+}
